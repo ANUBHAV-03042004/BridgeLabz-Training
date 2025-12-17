6a042b6aa2c18ca43a3a5a6ee35c0df584947cc8
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-17 16:10:38 +0530

    Add. printed welcome to bridgelabz

---
+++ core-java-practice/gcr-codebase/programming-elements/welcometobridgelabz/welcometobridgelabz.java
@@ -0,0 +1,11 @@
+package welcometobridgelabz;
+
+public class welcometobridgelabz {
+
+public static void main (String args[])
+{
+//	print welcome to bridgelabz
+	System.out.println("Welcome to BridgeLabz");
+	
+}
+}
