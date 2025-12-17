24a19ed6c7efb2fb90e816608eb7b293196b0763
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-17 14:57:44 +0530

    Add. Printed hello world

---
+++ core-java-practice/leet-code-codebase/helloworld/helloworld.java
@@ -0,0 +1,11 @@
+package helloworld;
+// program to print hello world
+public class helloworld {
+//	main function to execute program
+public static void main (String args[])
+{
+//	print hello world
+	System.out.println("Hello world");
+	
+}
+}
