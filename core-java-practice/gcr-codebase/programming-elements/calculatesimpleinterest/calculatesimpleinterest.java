d59d06c5c34afdbaf50a5e213ffa8bc4ff970d6e
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-17 16:13:40 +0530

    Add. created a program to calculate simple interest.

---
+++ core-java-practice/gcr-codebase/programming-elements/calculatesimpleinterest/calculatesimpleinterest.java
@@ -0,0 +1,12 @@
+package calculatesimpleinterest;
+// program to calculate simple interest
+public class calculatesimpleinterest {
+	  // Main function to execute program
+    public static void main(String[] args) {
+    	int principle=500;
+    	double rate=5,time=4.5;
+//    	calculate simple interest
+    	System.out.println("Simple interest of "+principle +","+rate+","+time +" principle , rate and time is :"+((principle*rate*time)/100));
+    }
+
+}
