4dc32fd715c718ef3a9018033cfc8147bc45cfb2
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-17 16:12:30 +0530

    Add. created a program to calculate area of circle.

---
+++ core-java-practice/gcr-codebase/programming-elements/areaofcircle/areaofcircle.java
@@ -0,0 +1,10 @@
+package areaofcircle;
+// program to calculate area of circle
+public class areaofcircle {
+		  // Main function to execute program
+	    public static void main(String[] args) {
+	    	int radius=5;
+//	    	calculated the area of circle
+	    	System.out.println("Area of circle with radius :"+radius+" is :"+ (22.0/7)*radius*radius);
+	    }
+	}
