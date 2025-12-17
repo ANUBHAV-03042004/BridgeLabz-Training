3fad1b56c552c83caff6c17d415d1b238f201b8e
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-17 15:15:42 +0530

    Add. Added 2 numbers

---
+++ core-java-practice/leet-code-codebase/addtwonumbers/addtwonumbers.java
@@ -0,0 +1,12 @@
+package addtwonumbers;
+// program to add 2 numbers
+public class addtwonumbers {
+//	main function to execute program
+public static void main (String args[])
+{
+	int first_number=5,second_number=4;
+//	print added number
+	System.out.println("The sum of "+first_number+" and "+second_number+" is "+(first_number+second_number));
+	
+}
+}
