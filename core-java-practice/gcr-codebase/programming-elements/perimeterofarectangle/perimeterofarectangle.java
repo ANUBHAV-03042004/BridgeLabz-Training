7260643a49f46c0cb836eb92f1706dcf435aca56
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-17 16:14:01 +0530

    Add. created a program to calculate perimeter of a rectangle.

---
+++ core-java-practice/gcr-codebase/programming-elements/perimeterofarectangle/perimeterofarectangle.java
@@ -0,0 +1,10 @@
+package perimeterofarectangle;
+//program to check perimeter of a rectangle
+public class perimeterofarectangle {
+	  // Main function to execute program
+    public static void main(String[] args) {
+    	int length=5,width=10;
+//    	calculate perimeter of a rectangle
+    	System.out.println("perimeter of a rectangle with "+length+","+width+" length and width is :"+(2*(length+width)));
+    }
+}
