f78bb0f7cd9f1b0eefd122f3e5f37a915d8b2df5
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-19 14:52:15 +0530

    [Anubhav Kumar Srivastava] Add. created a code to check whether a number is divisible by 5 or not

---
+++ core-java-practice/gcr-codebase/control-flows/checkifanumberisdivisibleby5/CheckIfANumberIsDivisibleBy5.java
@@ -0,0 +1,10 @@
+package checkifanumberisdivisibleby5;
+
+public class CheckIfANumberIsDivisibleBy5 {
+public static void main(String args[])
+{
+	int number_to_be_checked=11;
+	if(number_to_be_checked %5 ==0) System.out.printf("Is the number %d divisible by 5? Yes",number_to_be_checked);
+	else System.out.printf("Is the number %d divisible by 5? No",number_to_be_checked);
+}
+}
