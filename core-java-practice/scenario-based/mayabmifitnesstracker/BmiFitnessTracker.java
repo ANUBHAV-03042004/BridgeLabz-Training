3ab51881afeb6588e7464d7b792d196135090476
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-20 12:25:04 +0530

    [Anubhav Kumar Srivastava] Add. Solved maya's bmi calculator for clients.

--- core-java-practice/scenario-based/mayabmifitnesstracker/BmiFitnessTracker.java
+++ core-java-practice/scenario-based/mayabmifitnesstracker/BmiFitnessTracker.java
@@ -9,7 +9,7 @@ public static void main(String args[])
     double height=sc.nextDouble();
     System.out.println("Enter your weight in kgs :");
     double weight=sc.nextDouble();
-//    compute bmi
+//    compute bmi and tells whether you are overweight, normal , underweight or obese.
     double bmi=weight/(height*height);
     if(bmi>=18.5 && bmi<=24.9)System.out.println("You are normal");
     else if(bmi<18.5) System.out.println("You are underweight");
