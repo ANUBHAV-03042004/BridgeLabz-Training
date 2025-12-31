f680611acb0298b366dcfe127a78b68e4218efdc
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-12-31 11:47:28 +0530

    [Anubhav Kumar Srivastava] Add. Employee wage computation problem.

--- oops-practice/scenario-based/employeewagecomputationproblem/EmployeeWageComputationProblem.java
+++ oops-practice/scenario-based/employeewagecomputationproblem/EmployeeWageComputationProblem.java
@@ -1,5 +1,49 @@
 package employeewagecomputationproblem;
 
-public class EmployeeWageComputationProblem {
+import java.util.Random;
 
+public class EmployeeWageComputationProblem {
+    Random random = new Random();
+    String Empname;
+    int wageperhour = 20;
+    int totalwage = 0;
+
+    public String checkattendance(String Empname) {
+        int attendance_value = random.nextInt(2);
+        String attendance = (attendance_value == 1) ? "Present" : "Absent";
+        System.out.println(Empname + " is " + attendance);
+        return attendance;
+    }
+
+    public int findEmployeeStatus(String Empname, String attendance) {
+        int status_value = random.nextInt(2);
+        String empstatus="";
+        int EmployeehoursWorked=0;
+        switch (status_value) {
+            case 0:
+                empstatus = Empname + " is a Full Time Employee.";    
+                EmployeehoursWorked=8;
+                dailyEmployeeWage(attendance, 8);
+                break;
+            case 1:
+                empstatus = Empname + " is a Part Time Employee.";
+                EmployeehoursWorked=4;
+                dailyEmployeeWage(attendance, 4);
+                break;
+        }
+        System.out.println(empstatus);
+        return EmployeehoursWorked;
+    }
+
+    public void dailyEmployeeWage(String attendance, int hoursworked) {
+        if (attendance.equalsIgnoreCase("present")) {
+            int wage = wageperhour * hoursworked;
+            totalwage += wage;
+            System.out.println(Empname + " will get " + wage + " rs. as daily wage.");
+        }
+    }
+
+    public String calculateWageForAMonth() {
+        return Empname + " will get " + totalwage + " rs. as Monthly wage.";
+    }
 }
---
+++ oops-practice/scenario-based/employeewagecomputationproblem/Main.java
@@ -0,0 +1,24 @@
+package employeewagecomputationproblem;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Employee Name : ");
+        String Empname = sc.next();
+        
+        EmployeeWageComputationProblem employee = new EmployeeWageComputationProblem();
+        int totalworkingday = 0;
+        int totalhours = 0;
+
+        while (totalworkingday < 20 && totalhours < 100) {
+            String attendance = employee.checkattendance(Empname);
+            if (attendance.equalsIgnoreCase("present")) {
+                int hoursWorked = employee.findEmployeeStatus(Empname, attendance);
+                totalhours += hoursWorked;
+                totalworkingday++;
+            }
+        }
+        System.out.println(employee.calculateWageForAMonth());
+    }
+}
