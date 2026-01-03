31b2a8dadcd3143d2068cfd5fbcb3da42bb3121a
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2026-01-03 15:23:34 +0530

    [Anubhav Kumar Srivastava] Add. Completed ambulance route emergency patient navigation system.

--- dsa-practice/scenario-based/ambulancerouteemergencypatientnavigation/AmbulanceRouteEmergencyPatientNavigation.java
+++ dsa-practice/scenario-based/ambulancerouteemergencypatientnavigation/AmbulanceRouteEmergencyPatientNavigation.java
@@ -1,5 +1,53 @@
 package ambulancerouteemergencypatientnavigation;
 
-public class AmbulanceRouteEmergencyPatientNavigation {
+class CircularLinkedList {
+    String department;
+    String data;
+    CircularLinkedList next;
+
+    public CircularLinkedList(String department, String data) {
+        this.department = department;
+        this.data = data;
+    }
+}
+
+class AmbulanceRouteEmergencyPatientNavigation {
+    // RouteEmergencyPatientNavigation
+    CircularLinkedList head = null;
+
+    public void addroute(String department, String data) {
+        CircularLinkedList newNode = new CircularLinkedList(department, data);
+
+        if (head == null) {
+            head = newNode;
+            newNode.next = head;
+        } else {
+            CircularLinkedList temp = head;
+            while (temp.next != head) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            newNode.next = head;
+        }
+    }
+
+    public String bedAvailable() {
+        if (head == null) {
+            return "No routes available";
+        }
+
+        CircularLinkedList temp = head;
+
+        do {
+            if (!temp.data.equalsIgnoreCase("Full")) {
+                temp.data = "Full";
+                head = temp.next; // move head to next route
+                return "Bed available at : " + temp.department;
+            }
+            temp = temp.next;
+        } while (temp != head);
+
+        return "No beds available in any department";
+    }
 
 }
