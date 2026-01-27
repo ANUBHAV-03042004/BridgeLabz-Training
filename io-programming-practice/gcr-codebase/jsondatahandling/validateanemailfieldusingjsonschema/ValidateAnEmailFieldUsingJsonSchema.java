997ea3b35974c2cb552f981a7b1d262e6ac7a343
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2026-01-27 13:33:37 +0530

    [Anubhav Kumar Srivastava] Add. Updated code of jason data handling.

--- io-programming-practice/gcr-codebase/jsondatahandling/validateanemailfieldusingjsonschema/ValidateAnEmailFieldUsingJsonSchema.java
+++ io-programming-practice/gcr-codebase/jsondatahandling/validateanemailfieldusingjsonschema/ValidateAnEmailFieldUsingJsonSchema.java
@@ -1,11 +1,16 @@
-package validatejsonstructureusingjackson;
-import java.io.File;
+package validateanemailfieldusingjsonschema;
 
-import javax.management.RuntimeErrorException;
+import java.io.File;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-public class ValidateJsonUsingJackson {
+
+public class ValidateAnEmailFieldUsingJsonSchema {
+	static boolean EmailValidation(String email)
+	{
+		String regex ="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+		return email.matches(regex);
+	}
 public static void main(String[] args) {
 	String source = "C:\\Users\\HP\\eclipse-workspace\\BridgeLabz-Training\\io-programming-practice\\gcr-codebase\\jsondatahandling\\readajsonfileandprintallkeysandvalues\\student.json";
 	ObjectMapper mapper = new ObjectMapper();
@@ -15,28 +20,22 @@ public static void main(String[] args) {
 			  throw new RuntimeException("Invalid Json : Root element must be an array");
 		  }
 		  for(JsonNode node:rootNode) {
-			  if(!node.has("id")|| !node.get("id").isInt()) {
-				  throw new RuntimeException("Invalid or missing id");
-			  }
-		     if(!node.has("name")||!node.get("name").isTextual()) {
-		    	 throw new RuntimeException("Invalid or missing name.");
-			  }
-		     if(!node.has("name")||!node.get("name").isTextual()) {
-		    	 throw new RuntimeException("Invalid or missing name.");
-			  }
+			 
 		     if(!node.has("email")||!node.get("email").isTextual()) {
 		    	 throw new RuntimeException("Invalid or missing Email.");
 			  }
-		     int id = node.get("id").asInt();
-		     String name = node.get("name").asText();
 		     String email = node.get("email").asText();
-		     System.out.println("ID : "+ id);
-		     System.out.println("Name : "+ name);
+		      if(EmailValidation(email)) {
 		     System.out.println("Email : "+ email);
-		     System.out.println("Json Structure is Valid");
+		     System.out.println("Email is Valid");
+		      }
+		      else {  
+		    	  System.out.println("Email : "+ email);
+				     System.out.println("Email is InValid");
+				      }
 		  }
 	}catch(Exception e) {
-		System.out.println("Json Validation failed :"+ e.getMessage());
+		System.out.println("Email Validation failed :"+ e.getMessage());
 }
 	}
 }
