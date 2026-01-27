5ef443587017d37d14a98fcbff148fbf91fcb5ba
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2026-01-27 16:46:55 +0530

    [Anubhav Kumar Srivastava] Add. completed UC2 ability to add a new contact in address book.

--- oops-practice/gcr-codebase/addressbookproblem/addressbook/AddressBookMain.java
+++ oops-practice/gcr-codebase/addressbookproblem/addressbook/AddressBookMain.java
@@ -11,7 +11,20 @@ class AddressBookMain{
 		List<Contact> contacts = new ArrayList<>();
 		contacts.add(new Contact("Anubhav", "Sharma", "123 Main Street", "Chhata", "UP", "281401", "9876543210", "anubhav@example.com"));
 		contacts.add(new Contact("Priya", "Verma", "45 Park Lane", "Agra", "UP", "282001", "9123456780", "priya@example.com"));
-	
+		System.out.println("Enter the number of contacts to input : ");
+		int numOfContacts=sc.nextInt();
+		
+		while(numOfContacts-->0) {
+			 String firstName = sc.next();
+			    String lastName = sc.next();
+			    String address = sc.nextLine();
+			    String city=sc.next();
+			    String state=sc.next();
+			    String zip=sc.next();
+			    String phoneNumber=sc.next();
+			    String email=sc.next();
+			    contacts.add(new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email));
+		}
 		contacts.forEach(System.out::println);
 	}
 }
